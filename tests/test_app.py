3905e3df990847b6b34633428ca35bc6c02cd718
devisono <devisonlobo@gmail.com>
2024-07-18 18:10:50 -0300

    implementando a rota POST

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,22 +1,14 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_retornar_ok_e_hello_world():
-    client = TestClient(app)
-
+def test_root_retornar_ok_e_hello_world(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello world!'}
 
 
-def test_root_retornar_ok_e_hello_world_html():
-    client = TestClient(app)
-
+def test_root_retornar_ok_e_hello_world_html(client):
     response = client.get('/html')
 
     assert response.status_code == HTTPStatus.OK
@@ -33,3 +25,21 @@ def test_root_retornar_ok_e_hello_world_html():
     </html>
     """
     )
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@exemplo.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'alice',
+        'email': 'alice@exemplo.com',
+    }
