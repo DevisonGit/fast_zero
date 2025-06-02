fe8587bf5c46d8d8b167d0d619c0bf9fad0a5b1a
devisono <devison_santos@yahoo.com.br>
2025-06-02 19:17:45 -0300

    adicionando testes de filtro para o GET

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -43,3 +43,107 @@ async def test_list_todos_should_return_5_todos(session, client, user, token):
     )
 
     assert len(response.json()['todos']) == expected_todos
+
+
+@pytest.mark.asyncio
+async def test_list_todos_should_return_2_todos(session, client, user, token):
+    expected_todos = 2
+    session.add_all(TodoFactory.create_batch(5, user_id=user.id))
+    await session.commit()
+
+    response = client.get(
+        '/todos/?offset=1&limit=2',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert len(response.json()['todos']) == expected_todos
+
+
+@pytest.mark.asyncio
+async def test_list_todos_filter_should_return_5_todos(
+    session, user, client, token
+):
+    expected_todos = 5
+    session.add_all(
+        TodoFactory.create_batch(5, user_id=user.id, title='Test todo 1')
+    )
+    await session.commit()
+
+    response = client.get(
+        '/todos/?title=Test todo 1',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert len(response.json()['todos']) == expected_todos
+
+
+@pytest.mark.asyncio
+async def test_list_todos_description_should_return_5_todos(
+    session, user, client, token
+):
+    expected_todos = 5
+    session.add_all(
+        TodoFactory.create_batch(
+            5, user_id=user.id, description='Todos description'
+        )
+    )
+    await session.commit()
+
+    response = client.get(
+        '/todos/?description=Todos description',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert len(response.json()['todos']) == expected_todos
+
+
+@pytest.mark.asyncio
+async def test_list_todos_filter_state_should_return_5_todos(
+    session, user, client, token
+):
+    expected_todos = 5
+    session.add_all(
+        TodoFactory.create_batch(5, user_id=user.id, state=TodoState.draft)
+    )
+    await session.commit()
+
+    response = client.get(
+        '/todos/?state=draft', headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert len(response.json()['todos']) == expected_todos
+
+
+@pytest.mark.asyncio
+async def test_list_todos_filter_combined_should_return_5_todos(
+    session, user, client, token
+):
+    expected_todos = 5
+    session.add_all(
+        TodoFactory.create_batch(
+            5,
+            user_id=user.id,
+            title='Test todo combined',
+            description='combined description',
+            state=TodoState.done,
+        )
+    )
+
+    session.add_all(
+        TodoFactory.create_batch(
+            5,
+            user_id=user.id,
+            title='Other title',
+            description='other description',
+            state=TodoState.todo,
+        )
+    )
+
+    await session.commit()
+
+    response = client.get(
+        '/todos/?title=Test todo combined&description=combined&state=done',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert len(response.json()['todos']) == expected_todos
