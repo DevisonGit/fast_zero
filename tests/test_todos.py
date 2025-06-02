3c0801485e4e2bbefeec317fe77b814685b7d8f1
devisono <devison_santos@yahoo.com.br>
2025-06-01 22:10:38 -0300

    Implementando os endpoints todo POST e GET

---
+++ tests/test_todos.py
@@ -0,0 +1,45 @@
+import factory.fuzzy
+import pytest
+
+from fast_zero.models import Todo, TodoState
+
+
+class TodoFactory(factory.Factory):
+    class Meta:
+        model = Todo
+
+    title = factory.Faker('text')
+    description = factory.Faker('text')
+    state = factory.fuzzy.FuzzyChoice(TodoState)
+    user_id = 1
+
+
+def test_create_todo(client, token):
+    response = client.post(
+        '/todos',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'title': 'Test Todo',
+            'description': 'Test todo description',
+            'state': 'draft',
+        },
+    )
+    assert response.json() == {
+        'id': 1,
+        'title': 'Test Todo',
+        'description': 'Test todo description',
+        'state': 'draft',
+    }
+
+
+@pytest.mark.asyncio
+async def test_list_todos_should_return_5_todos(session, client, user, token):
+    expected_todos = 5
+    session.add_all(TodoFactory.create_batch(5, user_id=user.id))
+    await session.commit()
+
+    response = client.get(
+        '/todos', headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert len(response.json()['todos']) == expected_todos
