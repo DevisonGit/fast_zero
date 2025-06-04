ec2770576e1f7ba59739031d0c71c41ebe841567
devisono <devison_santos@yahoo.com.br>
2025-06-03 23:47:00 -0300

    Utilizando Docker na aplicação e inserindo o PostgreSQL

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ import pytest_asyncio
 from fastapi.testclient import TestClient
 from sqlalchemy import event
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.pool import StaticPool
+from testcontainers.postgres import PostgresContainer
 
 from fast_zero.app import app
 from fast_zero.database import get_session
@@ -49,11 +49,10 @@ def client(session):
 
 @pytest_asyncio.fixture
 async def session():
-    engine = create_async_engine(
-        'sqlite+aiosqlite:///:memory:',
-        connect_args={'check_same_thread': False},
-        poolclass=StaticPool,
-    )
+    with PostgresContainer('postgres:16', driver='psycopg') as postgres:
+        engine = create_async_engine(postgres.get_connection_url())
+        print(postgres.get_connection_url())
+
     async with engine.begin() as conn:
         await conn.run_sync(table_registry.metadata.create_all)
 
