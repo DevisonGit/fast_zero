bacf8362ea6fe024054b17d4b562bb604b61ad70
devisono <devison_santos@yahoo.com.br>
2025-12-06 18:02:55 -0300

    add: root hello world html

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schema import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'hello world'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_root_html():
+    return """
+    <html>
+        <head>
+            <title> our hello world </title>
+        </head>
+        <body>
+            <h1> Hello world </h1>
+        </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -15,3 +15,12 @@ def test_root_return_ok_and_hello_world():
     # Assert
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'hello world'}
+
+
+def test_rooot_return_ok_and_hello_world_html():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Hello world </h1>' in response.text
